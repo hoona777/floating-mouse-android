8be9001356f4d8ab6e7ed7542d58300f15c54660
hoona777 <sammmey555s@gmail.com>
2026-08-07 10:16:15 +0200

    Implement FloatingMouseAccessibilityService class

---
+++ app/src/main/java/com/floatingmouse/app/FloatingMouseAccessibilityService.kt
@@ -0,0 +1,30 @@
+package com.floatingmouse.app
+
+import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.AccessibilityServiceInfo
+import android.accessibilityservice.GestureDescription
+import android.graphics.Path
+
+class FloatingMouseAccessibilityService : AccessibilityService() {
+
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+        val info = AccessibilityServiceInfo().apply {
+            eventTypes = AccessibilityServiceInfo.TYPES_ALL_MASK
+            feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC
+            flags = AccessibilityServiceInfo.FLAG_DEFAULT or
+                    AccessibilityServiceInfo.FLAG_REQUEST_TOUCH_EXPLORATION_MODE
+        }
+        serviceInfo = info
+    }
+
+    fun injectTouchClick(x: Float, y: Float) {
+        val clickPath = Path().apply { moveTo(x, y) }
+        val stroke = GestureDescription.StrokeDescription(clickPath, 0, 50)
+        val gesture = GestureDescription.Builder().addStroke(stroke).build()
+        dispatchGesture(gesture, null, null)
+    }
+
+    override fun onAccessibilityEvent(event: android.view.accessibility.AccessibilityEvent?) {}
+    override fun onInterrupt() {}
+}
